2d02b5373a5da87a62e59a304ed80b33e70db174
lucasmalla <49912636+lucasmalla@users.noreply.github.com>
2024-09-13 06:02:41 +0300

    Delete site level data inputs directory

--- site level data inputs/2022 UNRWA/summary_code.R
+++
@@ -1,7 +0,0 @@
-rm(list = ls())
-library(readxl)
-setwd("~/Documents/Transferred files/Data/data part 4/WHO/Automated scripts version 3/site level data inputs/2022 UNRWA/West Bank")
-
-data_file = read_excel('raw_data.xlsx')
-
-table(data_file$q3)
