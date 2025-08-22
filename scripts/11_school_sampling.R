a61d733d208fab845c10d2e39ddd691cfdcc6580
lucasmalla <49912636+lucasmalla@users.noreply.github.com>
2025-08-22 08:44:08 +0100

    Update 11_school_sampling.R

--- scripts/11_school_sampling.R
+++ scripts/11_school_sampling.R
@@ -28,9 +28,9 @@
 ###
 class_sampling = function(numberOfclasses = 20, sampling_interval = 1.54, random_start = NULL) {
   #if (double_draw == 'Yes'){sample_size = 20} else(sample_size = 10)
-  #sample_size = round(0.75*numberOfclasses)
+  sample_size = round(0.75*numberOfclasses)
   ###
-  seq_numbers = seq(1, numberOfclasses, sampling_interval)
+  #seq_numbers = seq(1, numberOfclasses, sampling_interval)
   
   #if(length(seq_numbers) > sample_size)
   #{
